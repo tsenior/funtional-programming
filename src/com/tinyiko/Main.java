3ade19074786619f648fd2e064959a13c12c5041
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-15 10:33:02 +0200

    using lambdas

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -9,7 +9,23 @@ public class Main {
                 System.out.println("hi "+ name);
             }
         };
-
         greet.greet("Ntikelo");
+
+        GreetingMessage greeLambdas = (name -> {
+            System.out.println("hello " + name);
+        });
+
+        greeLambdas.greet("Tinyiko");
+
+        MessagePrinter message = (() -> {
+            System.out.println("Message from message printer");
+        });
+
+        MessagePrinter message2 = () -> {
+            System.out.println("seconf Message from message printer");
+        };
+
+        message.printMessage();
+        message2.printMessage();
     }
 }
---
+++ src/com/tinyiko/MessagePrinter.java
@@ -0,0 +1,7 @@
+package com.tinyiko;
+
+@FunctionalInterface
+public interface MessagePrinter {
+    public abstract void printMessage();
+}
+
